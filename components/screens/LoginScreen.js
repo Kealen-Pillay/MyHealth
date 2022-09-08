d34e2cfa1f5077849a0d11e902b73ca613937e7e
Kealen-Pillay <kealenpillay@gmail.com>
2022-09-09 11:58:52 +1200

    Setup login screen

---
+++ components/screens/LoginScreen.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import KeyboardAvoidingView from "react-native/Libraries/Components/Keyboard/KeyboardAvoidingView";
+
+const LoginScreen = () => {
+  const [patientID, setPatientID] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <Text>LoginScreen</Text>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#7FD8BE",
+    width: "100%",
+  },
+});
