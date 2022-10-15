535e8b07be02aab9164fb3529de6fe4006c226fc
Kealen-Pillay <kealenpillay@gmail.com>
2022-10-15 20:14:21 +1300

    Created left fragments of biometrics page

--- components/screens/BiometricsScreen.js
+++ components/screens/BiometricsScreen.js
@@ -5,7 +5,13 @@ import { theme } from "../../theme/theme";
 const BiometricsScreen = () => {
   return (
     <KeyboardAvoidingView style={styles.container}>
-      <Text>BiometricsScreen</Text>
+      <View style={styles.leftVerticalContainer}>
+        <View style={[styles.card, styles.heartCard]}></View>
+        <View style={[styles.card, styles.temperatureCard]}></View>
+      </View>
+      <View style={styles.rightVerticalContainer}></View>
+
+      <View style={styles.bottomContainer}></View>
     </KeyboardAvoidingView>
   );
 };
@@ -15,9 +21,26 @@ export default BiometricsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: theme.background,
     width: "100%",
   },
+  card: {
+    backgroundColor: theme.darkGrey,
+    borderRadius: 10,
+  },
+  leftVerticalContainer: {
+    height: "50%",
+    width: "50%",
+    marginTop: "15%",
+    marginLeft: "5%",
+  },
+  heartCard: {
+    width: "100%",
+    height: "60%",
+  },
+  temperatureCard: {
+    marginTop: "10%",
+    width: "100%",
+    height: "30%"
+  }
 });
