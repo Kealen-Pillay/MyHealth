305d9a258bcd4447c92486de546a865fd76d9b13
Kealen-Pillay <kealenpillay@gmail.com>
2022-10-15 20:40:25 +1300

    Added respiration container

--- components/screens/BiometricsScreen.js
+++ components/screens/BiometricsScreen.js
@@ -5,13 +5,19 @@ import { theme } from "../../theme/theme";
 const BiometricsScreen = () => {
   return (
     <KeyboardAvoidingView style={styles.container}>
-      <View style={styles.leftVerticalContainer}>
-        <View style={[styles.card, styles.heartCard]}></View>
-        <View style={[styles.card, styles.temperatureCard]}></View>
+      <View style={styles.topContainer}>
+        <View style={styles.leftVerticalContainer}>
+          <View style={[styles.card, styles.heartCard]}></View>
+          <View style={[styles.card, styles.temperatureCard]}></View>
+        </View>
+        <View style={styles.rightVerticalContainer}>
+          <View style={[styles.card, styles.hydrationContainer]}></View>
+          <View style={[styles.card, styles.bloodContainer]}></View>
+        </View>
       </View>
-      <View style={styles.rightVerticalContainer}></View>
-
-      <View style={styles.bottomContainer}></View>
+      <View style={styles.bottomContainer}>
+        <View style={[styles.card, styles.respirationContainer]}></View>
+      </View>
     </KeyboardAvoidingView>
   );
 };
@@ -21,26 +27,56 @@ export default BiometricsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: theme.background,
     width: "100%",
+    height: "100%",
   },
   card: {
     backgroundColor: theme.darkGrey,
     borderRadius: 10,
   },
-  leftVerticalContainer: {
+  topContainer: {
+    flexDirection: "row",
+    width: "100%",
     height: "50%",
+  },
+  leftVerticalContainer: {
+    height: "100%",
     width: "50%",
     marginTop: "15%",
-    marginLeft: "5%",
+    marginLeft: "1%",
   },
   heartCard: {
     width: "100%",
-    height: "60%",
+    height: "50%",
   },
   temperatureCard: {
-    marginTop: "10%",
+    marginTop: "5%",
     width: "100%",
-    height: "30%"
-  }
+    height: "30%",
+  },
+  rightVerticalContainer: {
+    height: "100%",
+    width: "45%",
+    marginTop: "15%",
+    marginLeft: "1%",
+  },
+  hydrationContainer: {
+    width: "100%",
+    height: "50%",
+  },
+  bloodContainer: {
+    marginTop: "5%",
+    width: "100%",
+    height: "30%",
+  },
+  bottomContainer: {
+    height: "100%"
+  },
+  respirationContainer: {
+    marginLeft: "1%",
+    width: "98%",
+    height: "20%",
+  },
 });
