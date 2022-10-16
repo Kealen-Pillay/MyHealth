3dd0f13a3d690717f59427368b4a79c9fd7ccbf5
Kealen-Pillay <kealenpillay@gmail.com>
2022-10-16 16:14:44 +1300

    Added conditionally rendered emergency alert

--- components/screens/EmergencyScreen.js
+++ components/screens/EmergencyScreen.js
@@ -5,33 +5,63 @@ import {
   KeyboardAvoidingView,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { theme } from "../../theme/theme";
 
 const EmergencyScreen = () => {
+  const [emergencyMessage, setEmergencyMessage] = useState("Inactive");
+
+  useEffect(() => {
+    setEmergencyMessage("Inactive");
+  }, []);
+
+  const handleCancel = () => {
+    setEmergencyMessage("Inactive");
+  };
+
+  const handleCall = () => {
+    setEmergencyMessage("Active");
+  };
+
   return (
     <KeyboardAvoidingView style={styles.container}>
       <View style={styles.emergencyHeaderContainer}>
         <Text style={styles.emergencyHeader}>Emergency</Text>
       </View>
-      <View style={styles.alertContainer}>
-        <View style={styles.emergencyTextContainer}>
-          <Text style={styles.emergencyText}>
-            Emergency Services Will Be Contacted In
-          </Text>
+      {emergencyMessage === "Inactive" ? (
+        <View style={styles.alertContainerSmall}>
+          <View style={styles.emergencyTextContainerSmall}>
+            <Text style={styles.emergencyText}>Contact Emergency Services</Text>
+          </View>
+          <View style={styles.buttonContainerSmall}>
+            <TouchableOpacity
+              style={[styles.callButton, styles.button, { width: "100%" }]}
+              onPress={handleCall}
+            >
+              <Text style={styles.buttonText}>Call</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.countdownContainer}>
-          <Text style={styles.countdownText}>5</Text>
+      ) : (
+        <View style={styles.alertContainerLarge}>
+          <View style={styles.emergencyTextContainerLarge}>
+            <Text style={styles.emergencyText}>
+              Emergency Services Will Be Contacted In
+            </Text>
+          </View>
+          <View style={styles.countdownContainer}>
+            <Text style={styles.countdownText}>5</Text>
+          </View>
+          <View style={styles.buttonContainerLarge}>
+            <TouchableOpacity
+              style={[styles.cancelButton, styles.button, { width: "100%" }]}
+              onPress={handleCancel}
+            >
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={[styles.cancelButton, styles.button]}>
-            <Text style={styles.buttonText}>Cancel</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={[styles.callButton, styles.button]}>
-            <Text style={styles.buttonText}>Call</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      )}
     </KeyboardAvoidingView>
   );
 };
@@ -56,13 +86,22 @@ const styles = StyleSheet.create({
     color: theme.text,
     fontSize: 50,
   },
-  alertContainer: {
+  alertContainerSmall: {
+    backgroundColor: theme.darkGrey,
+    width: "95%",
+    height: "30%",
+    borderRadius: 5,
+  },
+  alertContainerLarge: {
     backgroundColor: theme.darkGrey,
     width: "95%",
     height: "73%",
     borderRadius: 5,
   },
-  emergencyTextContainer: {
+  emergencyTextContainerSmall: {
+    height: "60%",
+  },
+  emergencyTextContainerLarge: {
     height: "30%",
   },
   emergencyText: {
@@ -76,20 +115,25 @@ const styles = StyleSheet.create({
     height: "50%",
     alignItems: "center",
     justifyContent: "center",
+    marginBottom: "5%"
   },
   countdownText: {
     color: theme.text,
     fontWeight: "bold",
     fontSize: 200,
   },
-  buttonContainer: {
+  buttonContainerSmall: {
     flex: 1,
-    flexDirection: "row",
     width: "100%",
-    alignItems: "center",
-    justifyContent: "space-between",
     paddingLeft: "5%",
     paddingRight: "5%",
+  },
+  buttonContainerLarge: {
+    flex: 1,
+    width: "100%",
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    justifyContent: "center"
   },
   cancelButton: {
     backgroundColor: "red",
